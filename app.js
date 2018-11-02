9669a5779f33bedb14a5f1cc0a6dcb15c7bcba57
Robb Bowes <up666724@myport.ac.uk>
2018-11-02 10:36:34 +0100

    Update app.js
    Dice roll event listener and image update

--- app.js
+++ app.js
@@ -8,3 +8,24 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+});
