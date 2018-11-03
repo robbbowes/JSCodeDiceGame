a66be29dd6b7090961cd34434400c6f8229f8a09
Robb Bowes <up666724@myport.ac.uk>
2018-11-03 16:43:17 +0100

    Fix winner and active player class on init

--- app.js
+++ app.js
@@ -11,18 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -54,6 +43,8 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     }
 });
 
+document.querySelector('.btn-new').addEventListener('click', init);
+
 function nextPlayer() {
     roundScore = 0;
     document.getElementById('current-' + activePlayer).textContent = '0';
@@ -63,3 +54,22 @@ function nextPlayer() {
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
     document.querySelector('.dice').style.display = 'none';
 };
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
